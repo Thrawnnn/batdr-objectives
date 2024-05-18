6656b0c8e11e0bf2446137c54f9129ae511dde96
Orion <145623338+Thrawnnn@users.noreply.github.com>
2024-05-18 11:00:20 -0500

    set stuff up

--- lua/autorun/sh_config.lua
+++ lua/autorun/sh_config.lua
@@ -1,8 +1,8 @@
 Config = Config or {} -- dont touch :)
 
-Config.NoRestrictTeams = {} -- Your teams go here, EX: Config.NoRestrictTeams = {TEAM_CITIZEN, TEAM_COP, TEAM_MAYOR}
-
+Config.NoRestrictTeams = {} -- Your teams that can post objectives go here, EX: Config.NoRestrictTeams = {TEAM_CITIZEN, TEAM_COP, TEAM_MAYOR}
 Config.Restrict = false -- Restrict the access to what teams you must be to use the objectives system 
+Config.UniqueTeam = false -- Whether or not the receiving end of the objective is team based. 
 
 -- I wouldn't touch the below function unless you know what you're doing :)
 
@@ -16,4 +16,8 @@ function CanSendObjective()
     else
         return true
     end
+end
+
+function CanSeeObjective()
+
 end
